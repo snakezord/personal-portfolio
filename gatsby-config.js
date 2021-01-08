16984ec93bb7a103121a3165f9629e10b956cabc
Roman Zhydyk <roman@student.dei.uc.pt>
2021-01-08 18:43:55 +0000

    Changed google analytics options

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+        head: true,
       },
     },
     {
