a4a22cfc8ac04d4f6beb25bcf819ee1ed1286b26
Roman Zhydyk <roman@student.dei.uc.pt>
2021-02-01 17:09:12 +0000

    Minor updates

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -7,7 +7,7 @@ import SocialLinks from "../social-links/social-links";
 const Sidebar = ({ profile, social }) => (
   <aside className="w-full lg:w-1/3 lg:border-r border-line lg:px-6 xl:px-12">
     <div className="flex flex-col h-full justify-between">
-      <div style={{ zIndex: 3 }}>
+      <div>
         <h2 className="font-header font-light text-front text-2xl leading-none mb-4">
           {profile.profession}
         </h2>
